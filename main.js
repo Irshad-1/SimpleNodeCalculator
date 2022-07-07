2d917faf4aa4288c41cc7708b1573e193cf3d32d
Md Irshad <md14015@gmail.com>
2022-07-07 11:43:15 +0530

    done

---
+++ main.js
@@ -0,0 +1,9 @@
+const add = require("./add");
+const subtract = require("./subtract");
+const multiply = require("./multiply");
+const divide = require("./divide");
+
+console.log("Addition of 5 and 4 is", add(5, 4));
+console.log("Subtraction of 2 from 10 is", subtract(10, 2));
+console.log("Multiply of 2 and 8 is", multiply(2, 8));
+console.log("Division 10 from 50 is", divide(50, 10));
